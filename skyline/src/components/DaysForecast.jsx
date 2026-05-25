cbba1777bbbaafff295ddb80ffa77e61c23525c2
Beca <234265987+becsnr@users.noreply.github.com>
2026-05-25 10:37:55 -0300

    previsão semana

--- skyline/src/components/DaysForecast.jsx
+++ skyline/src/components/DaysForecast.jsx
@@ -1,15 +1,36 @@
 import styles from './DaysForecast.module.css'
 
+import { FaTint } from 'react-icons/fa'
+
 function DaysForecast({ weather }) {
     return (
         <div className={styles.container}>
+
             {weather.map((day) => (
-                <div key={day.dt}>
-                    <p>{new Date(day.dt_txt).toLocaleDateString("pt-br", {weekday: "short" })}</p>
 
-                    <p>{Math.round(day.main.temp)}°</p>
+                <div key={day.dt} className={styles.card}>
+                    <h2 className={styles.day}>
+                        {new Date(day.dt_txt).toLocaleDateString("pt-br", {weekday: "short" })}
+                    </h2>
 
-                    <p>{day.weather[0].description}</p>
+                    {/* <p>{day.weather[0].description}</p> */}
+
+                    <img 
+                        className={styles.icon} 
+                        src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} 
+                        alt={day.weather[0].description} 
+                    />
+
+                    <p className={styles.temp}>
+                        {Math.round(day.main.temp)}°
+                    </p>
+
+                    <p className={styles.rain}>
+                        <FaTint />
+                        {Math.round(day.pop * 100)}%
+                    </p>
+
+                    
                 </div>
             ))}
         </div>
