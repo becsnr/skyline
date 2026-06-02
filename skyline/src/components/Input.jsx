cb94e5ce66c2cd43c4ce25eb5773f162e093a2ad
Beca <234265987+becsnr@users.noreply.github.com>
2026-06-02 09:04:21 -0300

    input

--- skyline/src/components/Input.jsx
+++ skyline/src/components/Input.jsx
@@ -1,12 +1,18 @@
 import styles from './Input.module.css'
 
+import { FaSearch } from "react-icons/fa";
+
 function Input({ city, setCity, handleSearch }) {
     return (
-        <>
-            <input className={styles.box} type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder='Buscar cidade' />
+        <section>
+            <div className={styles.inputContainer}>
+                <FaSearch />
+
+                <input className={styles.box} type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder='Buscar cidade' />
+            </div>
 
             <button onClick={handleSearch}>Buscar</button>
-        </>
+        </section>
     )
 }
 
